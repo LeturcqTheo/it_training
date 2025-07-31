c0021b7aff2b4fbbbf41d70273565753d5fd99a2
LeturcqTheo <leturcqtheopro@gmail.com>
2025-07-31 09:42:57 +0200

    Update Formateur.php

--- src/Entity/Formateur.php
+++ src/Entity/Formateur.php
@@ -2,41 +2,65 @@
 
 namespace App\Entity;
 
-use App\Repository\FormateurRepository;
+use App\Repository\FormationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: FormateurRepository::class)]
-class Formateur extends User
+#[ORM\Entity(repositoryClass: FormationRepository::class)]
+class Formation
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
     #[ORM\Column(length: 255)]
     private ?string $nom = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $prenom = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $cv = null;
+    private ?string $type = null;
 
     #[ORM\Column]
-    private ?bool $est_valide = null;
+    private ?float $prix = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $ficheFormation = null;
 
     /**
-     * @var Collection<int, Affecte>
+     * @var Collection<int, session>
      */
-    #[ORM\OneToMany(targetEntity: Affecte::class, mappedBy: 'formateur')]
-    private Collection $affectes;
+    #[ORM\OneToMany(targetEntity: Session::class, mappedBy: 'formation')]
+    private Collection $sessions;
+
+    /**
+     * @var Collection<int, Stagiaire>
+     */
+    #[ORM\OneToMany(targetEntity: Stagiaire::class, mappedBy: 'formation')]
+    private Collection $stagiaires;
+
+    /**
+     * @var Collection<int, soustheme>
+     */
+    #[ORM\ManyToMany(targetEntity: soustheme::class, inversedBy: 'formations')]
+    private Collection $sousthemes;
 
     public function __construct()
     {
-        $this->affectes = new ArrayCollection();
-        $this->setRoles(["ROLE_FORMATEUR"]);
+        $this->sessions = new ArrayCollection();
+        $this->stagiaires = new ArrayCollection();
+        $this->sousthemes = new ArrayCollection();
     }
 
     public function __toString(): string
     {
-        return $this->nom . ' ' . $this->prenom;
+        return $this->nom;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
     }
 
     public function getNom(): ?string
@@ -51,68 +75,122 @@ class Formateur extends User
         return $this;
     }
 
-    public function getPrenom(): ?string
+    public function getType(): ?string
     {
-        return $this->prenom;
+        return $this->type;
     }
 
-    public function setPrenom(string $prenom): static
+    public function setType(string $type): static
     {
-        $this->prenom = $prenom;
+        $this->type = $type;
 
         return $this;
     }
 
-    public function getCv(): ?string
+    public function getPrix(): ?float
     {
-        return $this->cv;
+        return $this->prix;
     }
 
-    public function setCv(string $cv): static
+    public function setPrix(float $prix): static
     {
-        $this->cv = $cv;
+        $this->prix = $prix;
 
         return $this;
     }
 
-    public function isEstValide(): ?bool
+    public function getFicheFormation(): ?string
     {
-        return $this->est_valide;
+        return $this->ficheFormation;
     }
 
-    public function setEstValide(bool $est_valide): static
+    public function setFicheFormation(string $ficheFormation): static
     {
-        $this->est_valide = $est_valide;
+        $this->ficheFormation = $ficheFormation;
 
         return $this;
     }
 
     /**
-     * @return Collection<int, Affecte>
+     * @return Collection<int, session>
      */
-    public function getAffectes(): Collection
+    public function getSessions(): Collection
     {
-        return $this->affectes;
+        return $this->sessions;
     }
 
-    public function addAffecte(Affecte $affecte): static
+    public function addSession(Session $session): static
     {
-        if (!$this->affectes->contains($affecte)) {
-            $this->affectes->add($affecte);
-            $affecte->setFormateur($this);
+        if (!$this->sessions->contains($session)) {
+            $this->sessions->add($session);
+            $session->setFormation($this);
         }
 
         return $this;
     }
 
-    public function removeAffecte(Affecte $affecte): static
+    public function removeSession(Session $session): static
     {
-        if ($this->affectes->removeElement($affecte)) {
+        if ($this->sessions->removeElement($session)) {
             // set the owning side to null (unless already changed)
-            if ($affecte->getFormateur() === $this) {
-                $affecte->setFormateur(null);
+            if ($session->getFormation() === $this) {
+                $session->setFormation(null);
             }
         }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Stagiaire>
+     */
+    public function getStagiaires(): Collection
+    {
+        return $this->stagiaires;
+    }
+
+    public function addStagiaire(Stagiaire $stagiaire): static
+    {
+        if (!$this->stagiaires->contains($stagiaire)) {
+            $this->stagiaires->add($stagiaire);
+            $stagiaire->setFormation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStagiaire(Stagiaire $stagiaire): static
+    {
+        if ($this->stagiaires->removeElement($stagiaire)) {
+            // set the owning side to null (unless already changed)
+            if ($stagiaire->getFormation() === $this) {
+                $stagiaire->setFormation(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, soustheme>
+     */
+    public function getSousthemes(): Collection
+    {
+        return $this->sousthemes;
+    }
+
+    public function addSoustheme(soustheme $soustheme): static
+    {
+        if (!$this->sousthemes->contains($soustheme)) {
+            $this->sousthemes->add($soustheme);
+        }
+
+        return $this;
+    }
+
+    public function removeSoustheme(soustheme $soustheme): static
+    {
+        $this->sousthemes->removeElement($soustheme);
 
         return $this;
     }
